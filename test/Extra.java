830b4a81441c02238a9c2d5d7fa3845dc87c725c
Karol Bułdak <s221451@student.uek.krakow.pl>
2023-01-11 12:26:42 +0100

    po test3

--- test/Extra.java
+++
@@ -1,5 +0,0 @@
-public interface Extra {
-    public abstract float discount();
-    public abstract float delivery();
-    public abstract float delivery(int tip);
-}
--- test/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Person p = new Person("Marek", "Wola");
-        System.out.println(p);
-
-    }
-}
